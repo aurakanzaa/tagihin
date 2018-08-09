933b00cbb6f9ef81df59847ee13b83a8823c862b
aurakanzaa <aurakanzaaa@gmail.com>
2018-08-09 14:59:57 +0700

    history

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
+import bModal from 'bootstrap-vue/es/components/modal/modal'
+import bModalDirective from 'bootstrap-vue/es/directives/modal/modal'
+
 import HelloWorld from '@/components/HelloWorld'
 import Tagihin from '@/components/Tagihin'
 import Paid from '@/components/Paid'
@@ -7,6 +11,18 @@ import Overdue from '@/components/Overdue'
 import Open from '@/components/Open'
 import History from '@/components/History'
 // import Test from '@/components/Test'
+
+// This imports <b-modal> as well as the v-b-modal directive as a plugin:
+import { Modal } from 'bootstrap-vue/es/components';
+Vue.use(Modal);
+
+// This imports <b-card> along with all the <b-card-*> sub-components as a plugin:
+import { Card } from 'bootstrap-vue/es/components';
+Vue.use(Card);
+
+// This imports directive v-b-scrollspy as a plugin:
+import { Scrollspy } from 'bootstrap-vue/es/directives';
+Vue.use(Scrollspy);
 
 Vue.use(Router)
 
